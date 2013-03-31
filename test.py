c427e6de518894fc510fc54241080eace5c9abb4
Marek Marecki <triviuss@gmail.com>
2013-04-01 01:34:16 +0200

    Modified tests

--- test.py
+++ test.py
@@ -12,13 +12,12 @@ class FormatterTests(unittest.TestCase):
         f.splitequalsign()
         self.assertEqual(f.argv, ["eggs", "--foo", "bar", "-s", "pam"])
 
-    def testSplittingShortOptionsWithArgumentWrappedIn(self):
+    def testSplittingLongOptionsWithArgumentWrapped(self):
         f = clap.Formatter(short="", long=["foo"], argv=["eggs", "--foo='bar baz'", "-s", "pam"])
         f._formatlongs()
         f.splitequalsign()
         self.assertEqual(f.argv, ["eggs", "--foo", "bar baz", "-s", "pam"])
 
-    def testSplittingShortOptionsWithArgumentWrappedInApostrophes(self):
         g = clap.Formatter(short="", long=["foo"], argv=["eggs", '--foo="bar baz"', "-s", "pam"])
         g._formatlongs()
         g.splitequalsign()
