8a802f0f5717e6987b8b9bf6370c48b4cab4ae74
Marek Marecki <marekjm@taistelu.com>
2013-08-04 22:40:10 +0200

    Initiali coding for builder

--- clap/builder.py
+++ clap/builder.py
@@ -1,5 +1,38 @@
 #!/usr/bin/env python3
 
+
 """This is the module responsible for building interfaces from
 files.
 """
+
+
+import json
+import os
+
+from clap import parser, modes
+
+
+class Builder():
+    """This object responsible for building interfaces.
+    """
+    def __init__(self, path, name=''):
+        self.path = path
+        self.name = name
+        self.data = None
+        self.interface = None
+
+    def load(self):
+        """Loads JSON from given path.
+        """
+        ifstream = open(self.path)
+        data = json.decode(ifstream.read())
+        ifstream.close()
+        self.data = data
+
+
+class Parser(Builder):
+    def build(self):
+        ui = parser.Parser()
+        for option in self.data:
+            ui.add(**option)
+        return ui
