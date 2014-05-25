5ea257f31d41089d4d77e29bd8e8afd425470069
Marek Marecki <marekjm@ozro.pw>
2014-05-25 21:17:35 +0200

    Updates to RedChecker

--- redclap/checker.py
+++ redclap/checker.py
@@ -12,23 +12,17 @@ Parser() to check correctness of input.
 class RedChecker():
     """This object is used for checking correctness of input.
     """
-    def __init__(self, args, parser):
+    def __init__(self, parser):
         self._parser = parser
-        self._args = args
 
     def _checkunrecognized(self):
         """Checks if input list contains any unrecognized options.
         """
         for i in self._parser._getinput():
-            if shared.lookslikeopt(i) and not self._parser.accepts(i): raise errors.UnrecognizedOptionError(i)
+            if shared.lookslikeopt(i) and not self._parser._mode.accepts(i): raise errors.UnrecognizedOptionError(i)
 
     def _checkarguments(self):
         """Checks if arguments given to options which require them are valid.
-        Raises `MissingArgumentError` when option which requires an argument is last item
-        in input list.
-        Raises `InvalidArgumentTypeError` when option is given argument of invalid type.
-        Raises `MissingArgumentError` when option which requires an argument is followed by
-        another option accepted by this instance of parser.
         **Notice:** if you want to pass option-like argument wrap it in `"` or `'` and
         escape first hyphen or double-escape first hyphen.
         """
@@ -36,16 +30,20 @@ class RedChecker():
         i = 0
         while i < len(input):
             opt = input[i]
-            if shared.lookslikeopt(opt) and self._parser.getopt(opt).type() is not None:
-                types = self._parser.getopt(opt).type()
+            if shared.lookslikeopt(opt) and self._parser._mode.getopt(opt).params():
+                types = self._parser._mode.getopt(opt).params()
                 if i+len(types) >= len(input):
+                    # missing parameters at the end of input
                     raise errors.MissingArgumentError('{0} ({1})'.format(opt, ', '.join([str(t)[8:-2] for t in types])))
-                if shared.lookslikeopt(input[i+1]) and self._parser.accepts(input[i+1]):
+                if shared.lookslikeopt(input[i+1]) and self._parser._mode.accepts(input[i+1]):
+                    # number of parameters too low before next option is passed
                     raise errors.MissingArgumentError(opt)
                 for n, atype in enumerate(types):
                     i += 1
                     try:
-                        atype(input[i])
+                        (atype if type(atype) is not str else self._parser._typehandlers[atype])(input[i])
+                    except KeyError:
+                        raise errors.UIDesignError('missing type handler "{0}" for option: {1}'.format())
                     except IndexError:
                         expected = ', '.join([str(t)[8:-2] for t in types])
                         got = ', '.join([str(t)[8:-2] for t in types[:n]])
@@ -57,7 +55,7 @@ class RedChecker():
     def _checkrequired(self):
         """Checks if all required options are present in input list.
         """
-        for option in self._parser._options:
+        for option in self._parser._mode.options():
             check = option['required']
             for n in option['not_with']:
                 if not check: break
