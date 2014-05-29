5150640d28830b897d2f18821478f424cf6904ed
Marek Marecki <marekjm@ozro.pw>
2014-05-30 01:33:48 +0200

    Added regex and function for checking valid mode-strings to shared module

--- redclap/shared.py
+++ redclap/shared.py
@@ -15,12 +15,15 @@ regex_opt_long  = re.compile('^' + longopt_base + '$')
 regex_longopt_with_equal_sign = re.compile('^' + longopt_base + '=.*$')
 regex_connected_shorts = re.compile('^-[a-zA-Z0-9][a-zA-Z0-9]+$')
 
+regex_mode = re.compile('^[A-Za-z]+(-?[A-Za-z]+)*$')
+
 
 def lookslikeopt(s):
     """Returns True if given string looks like option.
     """
-    return (regex_opt_short.match(s) or
-            regex_opt_long.match(s) or
-            regex_longopt_with_equal_sign.match(s) or
-            regex_connected_shorts.match(s)
-            ) is not None
+    return (regex_opt_short.match(s) or regex_opt_long.match(s) or regex_longopt_with_equal_sign.match(s) or regex_connected_shorts.match(s) ) is not None
+
+def lookslikemode(s):
+    """Returns true if given string looks like mode string.
+    """
+    return regex_mode.match(s) is not None
