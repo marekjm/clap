74d5a1205f25e621c206fa2cf605eb025a462afb
Marek Marecki <marekjm@ozro.pw>
2014-06-10 11:08:33 +0200

    Updated flat example

--- examples/flat.py
+++ examples/flat.py
@@ -19,9 +19,6 @@ mode = clap.builder.Builder(model).build().get()
 parser = clap.parser.Parser(mode).feed(args)
 checker = clap.checker.RedChecker(parser)
 
-print(argv, '->', args)
-print(base, filename)
-print(base, filename_ui)
 
 try:
     checker.check()
@@ -41,13 +38,29 @@ except clap.errors.RequiredOptionNotFoundError as e:
 except clap.errors.InvalidOperandRangeError as e:
     print('invalid number of operands: {0}'.format(e))
     fail = True
-except Exception as e:
+except clap.errors.UIDesignError as e:
+    print('UI has design error: {0}'.format(e))
     fail = True
-    raise e
+except Exception as e:
+    print('fatal: unhandled exception: {0}'.format(e))
+    fail = True
 finally:
     if fail: exit()
     else: ui = parser.parse().ui().finalise()
 
+
+if '--version' in ui:
+    print('using clap (RedCLAP) version {0}'.format(clap.__version__))
+    exit()
+
+if '--help' in ui:
+    helper = clap.helper.Helper(filename, mode)
+    helper.addUsage('--help')
+    helper.addUsage('--version')
+    helper.addUsage('--ok [opts...]')
+    helper.addUsage('--ok [--verbose | --quiet] [opts...]')
+    print(helper.gen().render())
+    exit(0)
 
 if '--echo' in ui:
     print(ui.get('-e'))
