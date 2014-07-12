0ae0a6f339f3d35f43da90e61afb9c4442cab141
Marek Marecki <marekjm@ozro.pw>
2014-07-13 00:26:54 +0200

    All CLAP-specific errors now inherit from base CLAPError

--- clap/errors.py
+++ clap/errors.py
@@ -2,37 +2,43 @@
 """
 
 
-class UnrecognizedOptionError(Exception):
+class CLAPError(Exception):
+    """Base class for all CLAP specific exceptions.
+    """
+    pass
+
+
+class UnrecognizedOptionError(CLAPError):
     """Raised when unrecognized option is found in input list.
     """
     pass
 
 
-class UnrecognizedCommandError(Exception):
+class UnrecognizedCommandError(CLAPError):
     """Raised when unrecognized command is found in input list.
     """
     pass
 
 
-class RequiredOptionNotFoundError(Exception):
+class RequiredOptionNotFoundError(CLAPError):
     """Raised when required option is not found in input list.
     """
     pass
 
 
-class WantedOptionNotFoundError(Exception):
+class WantedOptionNotFoundError(CLAPError):
     """Raised when even one of needed options is not found.
     """
     pass
 
 
-class MissingArgumentError(Exception):
+class MissingArgumentError(CLAPError):
     """Raised when option requires an argument but it is not found.
     """
     pass
 
 
-class InvalidArgumentTypeError(Exception):
+class InvalidArgumentTypeError(CLAPError):
     """Raised when option's argument cannot be converted to its desired type.
     Example: option `--point` requires two arguments: (int, int) but input is:
         foo --point 42 z
@@ -40,40 +46,48 @@ class InvalidArgumentTypeError(Exception):
     pass
 
 
-class MissingOperandError(Exception):
+class MissingOperandError(CLAPError):
     """Raised when required operand is missing.
     """
     pass
 
 
-class InvalidOperandTypeError(Exception):
+class InvalidOperandTypeError(CLAPError):
     """Raised when operand has invalid type.
     """
     pass
 
 
-class InvalidOperandRangeError(Exception):
+class InvalidOperandRangeError(CLAPError):
     """Raised when operand has invalid type.
     """
     pass
 
 
-class ConflictingOptionsError(Exception):
+class ConflictingOptionsError(CLAPError):
     """Raised when two or more conflicting options are found together.
     """
     pass
 
 
-class BuilderError(Exception):
+class BuilderError(CLAPError):
     """Raised when something wrong went in builder.
     """
     pass
 
 
-class UIDesignError(Exception):
+class UIDesignError(CLAPError):
     """Raised by checker when it finds out some error in UI design.
     For example:
     * option requires another option which is unrecognized (I did it once),
     * option uses type handler that is not registered in parser,
+    """
+    pass
+
+
+class FixMeError(CLAPError):
+    """Exception raised in places that shall be fixed before release.
+    Shall not be ever caught because it acts as a FIX-ME-NOW-I-AM-HERE
+    marker, and is not intended to be hidden.
     """
     pass
