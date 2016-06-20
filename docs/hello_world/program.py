80a4466e48118704e09bb933c2c1daa59d26375d
Marek Marecki <marekjm@ozro.pw>
2016-06-20 21:37:49 +0200

    Update doc

--- docs/hello_world/program.py
+++ docs/hello_world/program.py
@@ -35,3 +35,43 @@ args = list(clap.formatter.Formatter(sys.argv[1:]).format())
 #   -- --with="spam"
 #print('original: ', sys.argv[1:])
 #print('formatted:', args)
+
+# This will create the top-most "command" of the program, that will later be
+# matched to the input by the RedCLAP parser.
+command = clap.builder.Builder(model).build().get()
+
+# Create the parser that will analyse user input according to the command
+# built in previous line.
+# Immediately feed processed arguments to the parser.
+parser = clap.parser.Parser(command).feed(args)
+
+# Create a checker object that will check if the input matches the command.
+# This requires already parser to be already fed with arguments (which we
+# did when creating the parser).
+# Splitting the responsibilities of validating and parsing input between two
+# classes allows both of them to be simpler, and easier to extend and maintain.
+# Checker's only job is to check if the input is valid, and parser's only job
+# is to extract information from valid input.
+# The fact that Parser can safely assume that the input is always valid greatly
+# simplifies the code.
+checker = clap.checker.RedChecker(parser)
+
+try:
+    # This may throw many different exceptions.
+    # Each exception type conveys a precise message as to which part of the
+    # input is not valid, so that the error can be presented to the user to
+    # be corrected.
+    fail = True
+    checker.check()
+    fail = False
+except clap.errors.UnrecognizedOptionError as e:
+    # This exception is raised when an unreconised option is passed to the UI.
+    # It is triggered when a string that is looking like an option is given as
+    # one of the command line arguments, but can't be matched to any valid option.
+    print('unrecognized option found: {0}'.format(e))
+finally:
+    if fail: exit(1)
+    # Finally, if the input has been deemed valid, parse the command line
+    # arguments and return a UI object that can be used later in code
+    # to extract information about passed operands, options, and subcommands.
+    ui = parser.parse().ui().finalise()
